ca41ece06b8d450c364bc6bc8674ae0357dbedb8
AitezazAhmed <stoneghold4103@gmail.com>
2025-10-12 03:22:00 +0500

    project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+//import authRoutes from "./routes/authRoutes.js" 
+import {connectDB} from "./lib/db.js"
+dotenv.config()
+const app= express()
+app.use(cookieParser());
+const PORT=process.env.PORT || 5000
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+  })
+);
+app.use(express.json())
+//app.use("/api/auth",authRoutes)
+app.listen(PORT,()=>{
+    console.log("server is running",PORT)
+    connectDB()
+})
