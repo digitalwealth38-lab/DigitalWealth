920604bc813a6909b0113836de3cbca1e7a25864
AitezazAhmed <stoneghold4103@gmail.com>
2025-10-29 02:29:07 +0500

    update code

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: '100mb', extended: true }));
 const PORT=process.env.PORT || 5000
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://www.digitalwealthpk.com","hi"],
+    origin: ["http://localhost:5173","https://www.digitalwealthpk.com",],
     credentials: true
   })
 );
