cca2d22bbda4effaea413608eab0f81bacf99b05
AitezazAhmed <stoneghold4103@gmail.com>
2025-11-07 00:25:04 +0500

    update code

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -37,7 +37,7 @@ const platformBalanceAgg = await User.aggregate([
 const platformBalance = platformBalanceAgg[0]?.total || 0;
 // 💼 6. Admin profit = deposits - withdrawals - platformBalance  
 const adminProfit = totalDeposits - totalWithdrawals - platformBalance;
-    const totalEarnings = totalEarningsAgg[0]?.total || 0;
+
     console.log(
       totalUsers,
       totalDeposits,
