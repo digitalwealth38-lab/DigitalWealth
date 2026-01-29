d9f11e4d2d1472ca59e1d4fe54965d6282cfe797
AitezazAhmed <stoneghold4103@gmail.com>
2026-01-29 19:23:59 +0500

    update code

--- frontend/src/components/AdminManageUsers.jsx
+++ frontend/src/components/AdminManageUsers.jsx
@@ -120,7 +120,7 @@ const handleToggleWithdraw = async (id) => {
   return (
     <div className="min-h-screen bg-gray-50 py-10 px-4">
       <h2 className="text-3xl font-bold text-center text-sky-700 mb-8">
-        Admin – Manage u
+        Admin – Manage Us
       </h2>
 
       {/* SEARCH */}
