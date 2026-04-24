f068443a2f9e8b57f06ebad94cd16d7c2f04f933
AitezazAhmed <stoneghold4103@gmail.com>
2026-04-25 01:36:29 +0500

    update

--- backend/controllers/getSingleUserData.js
+++ backend/controllers/getSingleUserData.js
@@ -5,7 +5,7 @@ export const getSingleUserData = async (req, res) => {
   try {
     if (!req.user) return res.status(404).json({ message: "User not found" });
     const { name, investedBalance, totalEarnings, balance ,teamSize,directReferrals,level,rewards,currentPackage,referralCode  } = req.user;
-    res.json({ name, balance, totalEarnings, investedBalance,teamSize,directReferrals,level,rewards,currentPackage });
+    res.json({ name, balance, totalEarnings, investedBalance,teamSize,directReferrals,level,rewards,currentPackage,referralCode });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
